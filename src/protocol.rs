206dbe30ae2437ac5f9f477124432295443120c4
Razer <rayaneqarnaouipro@gmail.com>
2026-01-11 22:11:39 +0100

    fixing some skill issues

--- src/protocol.rs
+++ src/protocol.rs
@@ -13,46 +13,78 @@ pub unsafe fn init_moonwave_url_protocol() {
     let subclass: *mut Class = match Class::get("MoonwaveProtocol") {
         Some(c) => c as *const Class as *mut Class,
         None => {
-            let cls = unsafe { objc::runtime::objc_allocateClassPair(ns_url_protocol as *const _, subclass_name.as_ptr(), 0) };
-            unsafe { objc::runtime::objc_registerClassPair(cls) };
-            cls
+            unsafe { objc::runtime::objc_allocateClassPair(
+                ns_url_protocol as *const _,
+                subclass_name.as_ptr(),
+                0,
+            ) }
         }
     };
 
-    // idk how proper is this but atleast the compiler stfu'ed
-    unsafe {
-        // canInitWithRequest hook
-        let can_init_sel = sel!(canInitWithRequest:);
-        let can_init_imp: Imp = std::mem::transmute(can_init_with_request as unsafe extern "C" fn(*mut Class, Sel, *mut Object) -> bool);
-        objc::runtime::class_addMethod(
-            subclass,
-            can_init_sel,
-            can_init_imp,
-            b"B@:@\0".as_ptr() as *const i8,
-        );
-
-        // startLoading hook
-        let start_sel = sel!(startLoading);
-        let start_imp: Imp = std::mem::transmute(start_loading as unsafe extern "C" fn(*mut Object, Sel));
-        objc::runtime::class_addMethod(
-            subclass,
-            start_sel,
-            start_imp,
-            b"v@:\0".as_ptr() as *const i8,
-        );
-
-        // stopLoading hook (is this even needed?)
-        /*let stop_sel = sel!(stopLoading);
-        let stop_imp: Imp = std::mem::transmute(stop_loading as unsafe extern "C" fn(*mut Object, Sel));
-        objc::runtime::class_addMethod(
-            subclass,
-            stop_sel,
-            stop_imp,
-            b"v@:\0".as_ptr() as *const i8,
-        );*/
+    if subclass.is_null() {
+        return;
     }
 
+    // ud hooks the old one sucked
+    unsafe {
+        let meta = objc::runtime::object_getClass(subclass as *mut _) as *mut Class;
+    
+        // +canInitWithRequest:
+        objc::runtime::class_addMethod(
+            meta,
+            sel!(canInitWithRequest:),
+            std::mem::transmute(
+                can_init_with_request
+                    as unsafe extern "C" fn(*mut Class, Sel, *mut Object) -> bool
+            ),
+            b"B#:@\0".as_ptr() as *const i8,
+        );
+    
+        // +canInitWithTask:
+        objc::runtime::class_addMethod(
+            meta,
+            sel!(canInitWithTask:),
+            std::mem::transmute(
+                can_init_with_task
+                    as unsafe extern "C" fn(*mut Class, Sel, *mut Object) -> bool
+            ),
+            b"B#:@\0".as_ptr() as *const i8,
+        );
+    
+        // +canonicalRequestForRequest:
+        objc::runtime::class_addMethod(
+            meta,
+            sel!(canonicalRequestForRequest:),
+            std::mem::transmute(
+                canonical_request_for_request
+                    as unsafe extern "C" fn(*mut Class, Sel, *mut Object) -> *mut Object
+            ),
+            b"@#:@\0".as_ptr() as *const i8,
+        );
+    
+        // -startLoading
+        objc::runtime::class_addMethod(
+            subclass,
+            sel!(startLoading),
+            std::mem::transmute(
+                start_loading as unsafe extern "C" fn(*mut Object, Sel)
+            ),
+            b"v@:\0".as_ptr() as *const i8,
+        );
+    
+        // -stopLoading
+        objc::runtime::class_addMethod(
+            subclass,
+            sel!(stopLoading),
+            std::mem::transmute(
+                stop_loading as unsafe extern "C" fn(*mut Object, Sel)
+            ),
+            b"v@:\0".as_ptr() as *const i8,
+        );
+    }    
+
     // registering yay
+    unsafe { objc::runtime::objc_registerClassPair(subclass) };
     let _: () = msg_send![ns_url_protocol, registerClass: subclass];
 }
 
@@ -88,8 +120,26 @@ unsafe extern "C" fn can_init_with_request(_cls: *mut Class, _cmd: Sel, request:
     false
 }
 
+unsafe extern "C" fn can_init_with_task(cls: *mut Class, _cmd: Sel, task: *mut Object) -> bool {
+    if task.is_null() { return false; }
+    let req: *mut Object = msg_send![task, currentRequest];
+    if req.is_null() {
+        let req: *mut Object = msg_send![task, originalRequest];
+        if req.is_null() { return false; }
+        return unsafe { can_init_with_request(cls, sel!(canInitWithRequest:), req) };
+    }
+    unsafe { can_init_with_request(cls, sel!(canInitWithRequest:), req) }
+}
+
+unsafe extern "C" fn canonical_request_for_request(_cls: *mut Class, _cmd: Sel, request: *mut Object) -> *mut Object {
+    request
+}
+
 pub unsafe extern "C" fn start_loading(this: *mut Object, _cmd: Sel) {
-    let request: *mut Object = msg_send![this, request];
+    let og_req: *mut Object = msg_send![this, request];
+    if og_req.is_null() { return; }
+
+    let request: *mut Object = msg_send![og_req, mutableCopy];
     if request.is_null() { return; }
 
     let url: *mut Object = msg_send![request, URL];
@@ -117,11 +167,11 @@ pub unsafe extern "C" fn start_loading(this: *mut Object, _cmd: Sel) {
     }
 }
 
-/*#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn stop_loading(this: *mut Object, _cmd: Sel) {
-    let task: *mut Object = msg_send![this, task];
+    /*let task: *mut Object = msg_send![this, task];
     if !task.is_null() {
         let _: () = msg_send![task, cancel];
         let _: () = msg_send![this, setTask: ptr::null_mut::<Object>()];
-    }
-}*/
+    }*/
+}
